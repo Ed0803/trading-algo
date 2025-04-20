3461a1886f05604424967d9e7ae283a2113f54d9
Edhan0803 <edwardhan0803@gmail.com>
2025-04-20 16:23:03 +1000

    updated

--- data/ingest.py
+++ data/ingest.py
@@ -7,8 +7,8 @@ import yfinance as yf
 
 # Directories for raw and processed data
 BASE_DIR = Path(__file__).resolve().parents[2]
-RAW_DIR = BASE_DIR / "data" / "raw"
-PROCESSED_DIR = BASE_DIR / "data" / "processed"
+RAW_DIR = BASE_DIR / "algo-trading-project" / "data" / "raw"
+PROCESSED_DIR = BASE_DIR / "algo-trading-project" / "data" / "processed"
 
 
 def fetch_data(ticker: str,
